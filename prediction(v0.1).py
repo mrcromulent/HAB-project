55ecd85163ba04d9d0ad263a5be7a876809b515a
uripierreburmester <30432869+uripierreburmester@users.noreply.github.com>
2017-12-01 16:30:53 +1100

    Added notes
    nothing earth-shattering

--- prediction(v0.1).py
+++ prediction(v0.1).py
@@ -1,11 +1,29 @@
-import csv
-import os
-import numpy
+import numpy as np
+import matplotlib.pyplot as mpl
 
-with open('telemetry_test.csv', 'rb') as csvfile:
-    telem = list(csv.reader(csvfile))
 
-telemetry = [];
+data = np.genfromtxt('telemetry_test.txt',delimiter=',',skip_header=1)
 
-for i in range(0,len(telem)):
-    telemetry.append(telem[i][0].split(','))
+
+latitudes = data[:,][:,3]
+longitudes = data[:,][:,4]
+altitudes = data[:,][:,5]
+speeds = data[:,][:,6]
+headings = data[:,][:,7]
+
+
+
+#with open('telemetry_test.csv') as csvfile:   
+#    while True:   
+#        for line in csvfile:
+#            print(line)
+#        time.sleep(1)
+    
+    #telem = list(csv.reader(csvfile))
+
+#telemetry = [];
+
+#for i in range(0,len(telem)):
+#    telemetry.append(telem[i][0].split(','))
+    
+    
