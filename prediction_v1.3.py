b0b96d55f47dfb54e7527b93b23df74a6a45f70b
uripierreburmester <30432869+uripierreburmester@users.noreply.github.com>
2018-01-04 15:30:55 +1100

    Report first draft complete, added try,except failsafe

--- prediction_v1.3.py
+++ prediction_v1.3.py
@@ -29,7 +29,7 @@ fp = 'YERRALOON1_DATA//telemetry.txt'
 rising = False
 telemetry = []
 winds = []
-wind_band_width = 100
+wind_band_width = 5000
 sleep_time = 0.001 #seconds. Usually 1.
 prediction_gap = 0.09 #seconds.
 last_prediction_time = 0
@@ -55,62 +55,67 @@ while oc.false_telemetry(fp):
 
 while len(telemetry) < telemetry_cutoff:
     
-    #Record the start time of this loop
-    loop_start = t.time()
+    try:
     
-    #Add a new line to the telemetry list    
-    new_telemetry = oc.add_telemetry(fp)
-    
-    #Extract the the current altitude and state (state = [time,lat,long,alt,speed,heading,temp_ext,press,humidity])
-    state = new_telemetry[2:8] + new_telemetry[10:13]
-    alt = new_telemetry[5]
-    
-    #Detect when the balloon has started to lift off; set the lower wind band data
-    if rising == False and (alt - start_elev) > 100:
-        rising = True
-        wind_lower_data = state[:]
-            
-    if rising == True:
+        #Record the start time of this loop
+        loop_start = t.time()
         
-        #Add the new telemetry to the list of telemetry
-        telemetry.append(new_telemetry)
+        #Add a new line to the telemetry list    
+        new_telemetry = oc.add_telemetry(fp)
         
-        #If a wind band has been crossed ...
+        #Extract the the current altitude and state (state = [time,lat,long,alt,speed,heading,temp_ext,press,humidity])
+        state = new_telemetry[2:8] + new_telemetry[10:13]
+        alt = new_telemetry[5]
         
-        if (alt - wind_lower_data[3]) >= wind_band_width:
-            #...Make a new wind band if rising
-            [winds,wind_lower_data] = wind.make_new_band(state,wind_lower_data,winds)
+        #Detect when the balloon has started to lift off; set the lower wind band data
+        if rising == False and (alt - start_elev) > 100:
+            rising = True
+            wind_lower_data = state[:]
+                
+        if rising == True:
+            
+            #Add the new telemetry to the list of telemetry
+            telemetry.append(new_telemetry)
+            
+            #If a wind band has been crossed ...
+            
+            if (alt - wind_lower_data[3]) >= wind_band_width:
+                #...Make a new wind band if rising
+                [winds,wind_lower_data] = wind.make_new_band(state,wind_lower_data,winds)
+            
+            elif (wind_lower_data[3] - alt) >= wind_band_width:
+                #...Or refine the drag calculation if falling
+                wind_lower_data = landing.refine_drag_calculation(wind_lower_data,state,winds)           
+                
+                
+            #If sufficient time has passed, predict the landing site using splat
+            
+            if (t.time() - last_prediction_time) >= prediction_gap:
+                
+                prediction = landing.splat(state,winds)
+                last_prediction_time = t.time()
+                
+                #################################################
+                how_far_list.append(landing.how_far(prediction,state[0])[1])
+                times_list.append(landing.how_far(prediction,state[0])[0])
+                predictions_made += 1
+                ##################################################
         
-        elif (wind_lower_data[3] - alt) >= wind_band_width:
-            #...Or refine the drag calculation if falling
-            wind_lower_data = landing.refine_drag_calculation(wind_lower_data,state,winds)           
-            
-            
-        #If sufficient time has passed, predict the landing site using splat
+        #Check how long the loop took to execute. Put the code to sleep if necessary.
         
-        if (t.time() - last_prediction_time) >= prediction_gap:
-            
-            prediction = landing.splat(state,winds)
-            last_prediction_time = t.time()
-            
-            #################################################
-            how_far_list.append(landing.how_far(prediction,state[0])[1])
-            times_list.append(landing.how_far(prediction,state[0])[0])
-            predictions_made += 1
-            ##################################################
-    
-    #Check how long the loop took to execute. Put the code to sleep if necessary.
-    
-    calc_time = t.time() - loop_start
-    
-###################################################
-    calc_times.append(calc_time)        
-######################################################
-    
-    if calc_time < sleep_time:
-    
-        t.sleep(sleep_time - calc_time)
-    
+        calc_time = t.time() - loop_start
+        
+    ###################################################
+        calc_times.append(calc_time)        
+    ######################################################
+        
+        if calc_time < sleep_time:
+        
+            t.sleep(sleep_time - calc_time)
+        
+    except: #Failsafe. If any errors occur, the program exits
+        break
+        
 ##############################################################
 x = np.linspace(0,130,predictions_made)
 plt.xticks(x,times_list,rotation = 'vertical')
